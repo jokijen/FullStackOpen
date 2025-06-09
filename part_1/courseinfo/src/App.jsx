d2b2074cd80a80df259295a4491ad5a1a2df0a4b
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2025-06-09 21:00:04 +0300

    Add ex 1.2 kurssitiedot, step2

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -7,13 +7,22 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+
+  return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+  )
+}
+
 const Content = (props) => {
 
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -32,20 +41,17 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total exercise_total={exercises1 + exercises2 + exercises3}/>
+      <Content parts={parts}/>
+      <Total exercise_total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
