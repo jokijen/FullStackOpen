0ad32793243da93f873b6b9119e379280edb8a29
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2025-06-10 15:16:22 +0300

    Add ex 1.3 kurssitiedot, step3

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -20,9 +20,7 @@ const Content = (props) => {
 
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      <Part part={props.part} />
     </div>
   )
 }
@@ -41,17 +39,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
+
+  
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total exercise_total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Content part={part1}/>
+      <Content part={part2}/>
+      <Content part={part3}/>
+      <Total exercise_total={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
