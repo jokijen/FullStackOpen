d3ace17592c4b5eeea1a817f760ac8212a0e34de
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2025-06-19 15:20:47 +0300

    Add tasks 1.6.-1.9. unicafe basic structure of the the app with a separate component for Statistics

---
+++ part_1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  const average = ((props.good - props.bad) / all).toFixed(2)
+  const positive = (props.good / all * 100).toFixed(1)
+  
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <p>
+      good {props.good} <br />
+      neutral {props.neutral} <br />
+      bad {props.bad} <br />
+      all {all} <br />
+      average {average} <br />
+      positive {positive} %
+    </p>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+
+      </p>
+    </div>
+  )
+}
+
+export default App
