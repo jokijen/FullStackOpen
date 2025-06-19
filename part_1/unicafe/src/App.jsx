e99c4a91f92f76591512bbaa1c41a27b7d872858
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2025-06-19 16:33:29 +0300

    Add tasks 1.10.-1.11. unicafe: Add separate components for Button and StatisticLine, refactor, display stats in a table

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad
-  const average = ((props.good - props.bad) / all).toFixed(2)
-  const positive = (props.good / all * 100).toFixed(1)
-  
-  if (all === 0) {
+  if (props.all === 0) {
     return <p>No feedback given</p>
   }
 
+  return(
+    <table>
+    <tbody>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="average" value={props.average.toFixed(2)} />
+      <StatisticLine text="positive" value={props.positive.toFixed(1) + ' %'} />
+    </tbody>
+    </table>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = (props) => {
   return (
-    <p>
-      good {props.good} <br />
-      neutral {props.neutral} <br />
-      bad {props.bad} <br />
-      all {all} <br />
-      average {average} <br />
-      positive {positive} %
-    </p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -26,18 +34,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad  = () => setBad(bad + 1)
+
+  const all = good + neutral + bad
+  const average = all !== 0 ? (good - bad) / all : 0
+  const positive = all !== 0 ? (good / all * 100) : 0
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={incrementGood} text='good' />
+      <Button onClick={incrementNeutral} text='neutral' />
+      <Button onClick={incrementBad} text='bad' />
       <h1>statistics</h1>
-      <p>
-        <Statistics good={good} neutral={neutral} bad={bad} />
-
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
   )
 }
