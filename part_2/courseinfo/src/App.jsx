0f45ee4c4c8e3200764f1b3d321361a8174f97df
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2025-06-22 15:06:06 +0300

    Add part 2, ex.2.1: courseinfo. Add component course and ensure app works with any quantity of parts for a course.

---
+++ part_2/courseinfo/src/App.jsx
@@ -0,0 +1,66 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+  )
+}
+
+const Content = (props) => {
+  const parts = [...props.parts]
+
+  return (
+    <div>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header name={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
